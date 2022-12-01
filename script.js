d943e338768779d765b969fa793b400c6a75aae0
alsiesta <alsiesta@gmail.com>
2022-12-01 14:15:01 +0100

    first commit Alsiesta: summary.html and dependencies

---
+++ script.js
@@ -0,0 +1,20 @@
+async function init() {
+    await includeHTML();
+    document.getElementById('headline').innerHTML = 'Herzlich willkommen!!';
+}
+
+
+async function includeHTML() {
+    let includeElements = document.querySelectorAll('[w3-include-html]');
+    for (let i = 0; i < includeElements.length; i++) {
+        const element = includeElements[i];
+        file = element.getAttribute("w3-include-html"); // "includes all code that is wrapped in the <div> with the specified attribute"
+        let resp = await fetch(file);
+        if (resp.ok) {
+            element.innerHTML = await resp.text();
+        } else {
+            element.innerHTML = 'Page not found';
+        }
+    }
+
+}
