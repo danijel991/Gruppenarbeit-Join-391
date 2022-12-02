af47df0dcb9d8322319091cea24b15dba1e05afc
corneliuzediu <zediucorneliu@gmail.com>
2022-12-02 15:55:52 +0100

    Add Forgot Password HTML

--- src/js/logIn_register.js
+++ src/js/logIn_register.js
@@ -1,22 +1,56 @@
-function initLogo(){
+function initLogo() {
     changeBg();
     changeLogo();
     minimiseLogo();
 }
 
 
-function minimiseLogo(){
+function minimiseLogo() {
     let container = document.getElementById('logo_container');
     container.style.top = "7rem"
     container.style.left = "7rem"
 }
 
 
-function changeBg(){
+function changeBg() {
     let container = document.getElementById('logo_container');
     container.style.backgroundColor = "unset";
 }
 
-function changeLogo(){
-    document.getElementById('logo__img').src= "/src/img/logo_blue.png";
+function changeLogo() {
+    document.getElementById('logo__img').src = "/src/img/logo_blue.png";
+}
+
+/***    Log In  ***/
+function logInUser() {
+    let emailUser = document.getElementById('email');
+    let passwordUser = document.getElementById('password');
+    checkIfExists(emailUser, passwordUser);
+
+}
+
+// Check if the user exists
+function checkIfExists(emailUser, passwordUser) {
+    /*for (let i = 0; i < emailArray.length; i++) {
+        if (emailUser == emailArray[i] && passwordUser == passwordArray[i]) {
+            toSummaryPage();
+        } else if {
+            ###     // User not registered! Ask to register
+        }
+    }*/
+    return false
+}
+
+
+// Guest User Function
+function logInUserGuest() {
+    let guestEmail = "guestemail@gmail.com"
+    let guestPassword = "guestpassword"
+    document.getElementById('email').value = guestEmail;
+    document.getElementById('password').value = guestPassword;
+    toSummaryPage();
+}
+
+function toSummaryPage() {
+    window.open('http://192.168.178.59:5500/src/html/summary.html', '_self');
 }
