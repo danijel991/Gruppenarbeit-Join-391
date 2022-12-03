1b169d34d6ed7370e07a585de036d4a97485c2a8
HanamiGeto <tidu11@hotmail.de>
2022-12-03 15:47:06 +0100

    sidebar updated

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
 async function init() {
-    initLogo();
+    // initLogo();
     await includeHTML();
 }
 
---
+++ src/js/sidebar.js
@@ -0,0 +1,24 @@
+function active(id) {
+    document.getElementById(id).classList.add('active');
+    if (id == 'board') {
+        removeActive('summary', 'task', 'contacts', 'legal-notice');
+    } else if (id == 'task') {
+        removeActive('summary', 'board', 'contacts', 'legal-notice');
+    } else if (id == 'contacts') {
+        removeActive('summary', 'board', 'task', 'legal-notice');
+    } else if (id == 'legal-notice') {
+        document.getElementById(id).classList.add('second-active');
+        removeActive('summary', 'board', 'contacts', 'task');
+    } else if (id == 'summary') {
+        removeActive('task', 'board', 'contacts', 'legal-notice');
+    }
+    
+}
+
+
+function removeActive(id, id2, id3, id4) {
+    document.getElementById(id).classList.remove('active');
+    document.getElementById(id2).classList.remove('active');
+    document.getElementById(id3).classList.remove('active');
+    document.getElementById(id4).classList.remove('active');
+}
