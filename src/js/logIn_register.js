49896bf7cde8313236d141a7dee18969ac860033
corneliuzediu <zediucorneliu@gmail.com>
2022-12-10 17:24:21 +0100

    checkbox - not finished

--- src/js/logIn_register.js
+++ src/js/logIn_register.js
@@ -2,53 +2,74 @@
 let indexActiveUser;
 let indexReset;
 
+
+
 function logoAnimation() {
-  transitionLogo();
-  setTimeout(changeBg, 350);
-  setTimeout(showCardAndHeader, 400);
-  startPage();
+    transitionLogo();
+    setTimeout(changeBg, 350);
+    setTimeout(showCardAndHeader, 400);
+    startPage();
+    callCheckBox();
 }
+
+function callCheckBox(){
+    let checkbox = document.getElementById('rememberMe');
+    checkbox.addEventListener('change', e=>{
+        if(e.target.checked){
+            checkbox.setAttribute('checked', true );
+            console.log(e.target.checked);
+        } else{
+            checkbox.removeAttribute('checked');
+            console.log(e.target.checked);
+        }
+        
+    })
+}
+
+
+
+
 
 // Function for the Animation
 function changeBg() {
-  document.getElementById("initial__file--wrapper").style.background = "white";
+    document.getElementById("initial__file--wrapper").style.background = "white";
 }
 
 function transitionLogo() {
-  document.getElementById("logo__img").style.left = "0px";
-  document.getElementById("logo__img").style.top = "0px";
-  document.getElementById("logo__img").style.scale = "1";
-  document.getElementById("logo__img").style.opacity = "0";
+    document.getElementById("logo__img").style.left = "0px";
+    document.getElementById("logo__img").style.top = "0px";
+    document.getElementById("logo__img").style.scale = "1";
+    document.getElementById("logo__img").style.opacity = "0";
 }
 
 function showCardAndHeader() {
-  document.getElementById("logIn__frame").style.display = "flex";
-  document.getElementById("to__sign-in--wrapper").style.display = "flex";
+    document.getElementById("logIn__frame").style.display = "flex";
+    document.getElementById("to__sign-in--wrapper").style.display = "flex";
 }
 
 /***    Log In  ***/
 async function logInUser() {
-  let emailUser = document.getElementById("email").value;
-  let passwordUser = document.getElementById("password").value;
-  let acces = await checkIfExists(emailUser, passwordUser);
-  goToSummary(acces);
-  emailUser.value = "";
-  passwordUser = "";
+    let emailUser = document.getElementById("email").value;
+    let passwordUser = document.getElementById("password").value;
+    let acces = await checkIfExists(emailUser, passwordUser);
+    goToSummary(acces);
+    emailUser.value = "";
+    passwordUser = "";
 }
 
 // Check if the user exists
 async function checkIfExists(emailUser, passwordUser) {
-  let emailArray = usersArray.map((email) => email.userEmail);
-  let passwordArray = usersArray.map((password) => password.userPassword);
-  let findEmail = emailArray.find((email) => email == emailUser);
-  let findPassword = passwordArray.find((password) => password == passwordUser);
-  if (findEmail === undefined || findPassword === undefined) {
-    await setActiveUser(emailUser);
-    return false;
-  } else {
-    await setActiveUser(emailUser);
-    return true;
-  }
+    let emailArray = usersArray.map((email) => email.userEmail);
+    let passwordArray = usersArray.map((password) => password.userPassword);
+    let findEmail = emailArray.find((email) => email == emailUser);
+    let findPassword = passwordArray.find((password) => password == passwordUser);
+    if (findEmail === undefined || findPassword === undefined) {
+        await setActiveUser(emailUser);
+        return false;
+    } else {
+        await setActiveUser(emailUser);
+        return true;
+    }
 }
 
 async function setActiveUser(userEmail) {
@@ -71,30 +92,30 @@ async function logInActiveUser() {
 }
 
 function goToSummary(acces) {
-  if (acces == true) {
-    toSummaryPage();
-  } else {
-    alert("User not registered");
-  }
+    if (acces == true) {
+        toSummaryPage();
+    } else {
+        alert("User not registered");
+    }
 }
 
 async function logOut() {
-  activeUser["quickAcces"] = false;
-  await saveLocalActiveUser(activeUser);
-  toLogInPage();
+    activeUser["quickAcces"] = false;
+    await saveLocalActiveUser(activeUser);
+    toLogInPage();
 }
 
 // Guest User Function
 function logInUserGuest() {
-  let guestEmail = "guestemail@gmail.com";
-  let guestPassword = "guestpassword";
-  document.getElementById("email").value = guestEmail;
-  document.getElementById("password").value = guestPassword;
-  logInUser();
+    let guestEmail = "guestemail@gmail.com";
+    let guestPassword = "guestpassword";
+    document.getElementById("email").value = guestEmail;
+    document.getElementById("password").value = guestPassword;
+    logInUser();
 }
 
 function toSummaryPage() {
-  location.href = "./src/html/summary.html";
+    location.href = "./src/html/summary.html";
 }
 
 // Sign Up Functions
@@ -132,50 +153,50 @@ async function getUserInfo() {
 }
 
 async function addToDatabase(newUser, newEmail) {
-        usersArray.push(newUser);
-        await saveInBackend();
-        showConfirmation();
+    usersArray.push(newUser);
+    await saveInBackend();
+    showConfirmation();
 }
 
 function checkIfAlreadyRegistered(newEmail) {
-  let emailArray = usersArray.map((email) => email.userEmail);
-  let findEmail = emailArray.find((email) => email == newEmail);
-  return findEmail;
+    let emailArray = usersArray.map((email) => email.userEmail);
+    let findEmail = emailArray.find((email) => email == newEmail);
+    return findEmail;
 }
 
 function cleanInput() {
-  document.getElementById("newUser-name").value = "";
-  document.getElementById("newUser-email").value = "";
-  document.getElementById("newUser-password").value = "";
+    document.getElementById("newUser-name").value = "";
+    document.getElementById("newUser-email").value = "";
+    document.getElementById("newUser-password").value = "";
 }
 
 function getColor() {
-  let r = Math.floor(Math.random() * 256);
-  let g = Math.floor(Math.random() * 256);
-  let b = Math.floor(Math.random() * 256);
-  let color = `rgb(${r}, ${g}, ${b})`;
-  return color;
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    let color = `rgb(${r}, ${g}, ${b})`;
+    return color;
 }
 
 function getInitials(newName) {
-  var names = newName.split(" "),
-    initials = names[0].substring(0, 1).toUpperCase();
-  if (names.length > 1) {
-    initials += names[names.length - 1].substring(0, 1).toUpperCase();
-  } else if (names.length == 1) {
-    initials = newName.substring(0, 2).toUpperCase();
-  }
-  return initials;
+    var names = newName.split(" "),
+        initials = names[0].substring(0, 1).toUpperCase();
+    if (names.length > 1) {
+        initials += names[names.length - 1].substring(0, 1).toUpperCase();
+    } else if (names.length == 1) {
+        initials = newName.substring(0, 2).toUpperCase();
+    }
+    return initials;
 }
 
-function showConfirmation(){
+function showConfirmation() {
     document.getElementById('signUp__id--main').classList.add('blur');
     document.getElementById('signUp__id--response').classList.remove('d-none')
     document.getElementById('signUp__id--response').style.transform = "translateY(-20vh)"
 }
 
 
-function resetConfirmation(){
+function resetConfirmation() {
     document.getElementById('signUp__id--main').classList.remove('blur');
     document.getElementById('signUp__id--response').classList.add('d-none')
     document.getElementById('signUp__id--response').style.transform = "translateY(0vh)"
@@ -184,15 +205,15 @@ function resetConfirmation(){
 
 /*** Reset Password functions ***/
 function toResetPassword() {
-  let emailUser = document.getElementById("forgot__password--email").value;
-  let index = checkIfEmailExists(emailUser);
-  indexReset = getIndex(index);
-  allowResetPassword(index);
-  return indexReset;
+    let emailUser = document.getElementById("forgot__password--email").value;
+    let index = checkIfEmailExists(emailUser);
+    indexReset = getIndex(index);
+    allowResetPassword(index);
+    return indexReset;
 }
 
 function getIndex(index) {
-  return index;
+    return index;
 }
 
 async function resetPasswordUser(indexReset) {
@@ -210,29 +231,29 @@ async function resetPasswordUser(indexReset) {
 }
 
 function changeDivReset() {
-  document.getElementById("forgot__request").classList.add("d-none");
-  document.getElementById("forgot__newPassword").classList.remove("d-none");
+    document.getElementById("forgot__request").classList.add("d-none");
+    document.getElementById("forgot__newPassword").classList.remove("d-none");
 }
 
 function clearInputNewPassword() {
-  document.getElementById("user__newPassword-1").value = "";
-  document.getElementById("user__newPassword-2").value = "";
+    document.getElementById("user__newPassword-1").value = "";
+    document.getElementById("user__newPassword-2").value = "";
 }
 
 function checkIfEmailExists(emailUser) {
-  let emailArray = usersArray.map((email) => email.userEmail);
-  let findEmailIndex = emailArray.findIndex((email) => email == emailUser);
-  return findEmailIndex;
+    let emailArray = usersArray.map((email) => email.userEmail);
+    let findEmailIndex = emailArray.findIndex((email) => email == emailUser);
+    return findEmailIndex;
 }
 
 function allowResetPassword(index) {
-  if (index >= 0) {
-    changeDivReset();
-  } else {
-    alert("Email not registered");
-  }
+    if (index >= 0) {
+        changeDivReset();
+    } else {
+        alert("Email not registered");
+    }
 }
 
 function toLogInPage() {
-  window.location.href = "./../../index.html";
+    window.location.href = "./../../index.html";
 }
