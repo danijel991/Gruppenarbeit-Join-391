48236303c3383aa0974ae6e8497f94d685ca30b4
alsiesta <as@daslizenzatelier.de>
2022-12-12 23:29:34 +0100

    update: edit contact dialog

--- src/js/contacts.js
+++ src/js/contacts.js
@@ -1,3 +1,4 @@
+/*/////////////////////// ADD NEW CONTACT START ////////////////////////////////*/
 function openAddContactDialog() {
   document.getElementById("overlay").classList.remove("d-none");
 
@@ -13,11 +14,77 @@ function closeAddContactDialog() {
     document.getElementById("overlay").classList.add("d-none");
   }, 200);
 }
+/*/////////////////////// ADD NEW CONTACT END ////////////////////////////////*/
+/*/////////////////////// EDIT CONTACT START ////////////////////////////////*/
+function openEditContactDialog(index) {
+  document.getElementById("overlay2").classList.remove("d-none");
+  let allContacts = usersArray[activeUser["userID"]]["userContacts"];
+  let contact = allContacts[index];
+  const { name, initials, initialsColor, email, phone } = contact;
+  let content = document.getElementById("edit-contact-modal");
+  content.innerHTML = "";
+  content.innerHTML = `
+  <div class="contact-dialog-top">
+                    <img class="close-icon" src="../img/close_icon.png" onclick="closeEditContactDialog()">
+                    <img src="../img/join-logo.png">
+                    <h2 class="contact-title" id="exampleModalLabel">Edit contact</h2>
 
+                    <h4 id="info-text" class="info-text">Tasks are better with a team!</h4>
+
+                </div>
+                <div class="contact-dialog-bottom">
+                    <div class="user-avatar"><img src="../img/user-avatar.png" alt=""></div>
+                    <div class="form">
+                        <form class="add-contact_form" onsubmit="editUserContact(); return false;">
+                            <div class="add-contact-input-field">
+                                <input id="edit-contact-name" class="contact-form-control contacts_input" type="text"
+                                    placeholder="Name" required>
+                                <img src="/src/img/input_name.png" alt="">
+                            </div>
+                            <div class="add-contact-input-field">
+                                <input id="edit-contact-email" class="contact-form-control contacts_input " type="email"
+                                    placeholder="Email" required>
+                                <img src="/src/img/input_mail.png" alt="">
+                            </div>
+                            <div class="add-contact-input-field">
+                                <input id="edit-contact-phone" class="contact-form-control contacts_input " type="number"
+                                    pattern="" placeholder="Phone" required>
+                                <img src="/src/img/phone_icon.png" alt="">
+                            </div>
+                            <div class="edit-contact-buttons">
+                            <button class="delete-contact-button" required>
+                                <span>Delete</span><img src="../img/addcontact.png">
+                            </button>
+                            <button class="edit-contact-button" required>
+                                <span>Save</span><img src="../img/addcontact.png">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                `;
+                document.getElementById("edit-contact-name").value = `${name}`;
+                document.getElementById("edit-contact-email").value = `${email}`;
+                document.getElementById("edit-contact-phone").value = `${phone}`;
+
+  setTimeout(() => {
+    document.getElementById("edit-contact-modal").classList.add("slide-in");
+  }, 10);
+}
+
+function closeEditContactDialog() {
+  document.getElementById("edit-contact-modal").classList.remove("slide-in");
+
+  setTimeout(() => {
+    document.getElementById("overlay2").classList.add("d-none");
+  }, 200);
+}
+
+function editUserContact(index) {}
+/*/////////////////////// EDIT CONTACT END ////////////////////////////////*/
 async function loadAllContacts() {
   await init();
 
-  let allContacts = usersArray[activeUser['userID']]["userContacts"];
+  let allContacts = usersArray[activeUser["userID"]]["userContacts"];
   let content = document.getElementById("contact-list");
   content.innerHTML = " ";
 
@@ -81,26 +148,26 @@ function setColorForInitial(initials) {
     number = number + letterNumber;
   }
   let remainder = number % 5;
- if (remainder === 0) {
-   return 'rgb(221,70,60)';
- } else if (remainder === 1) {
-   return 'rgb(252,188,201)';
- } else if (remainder === 2) {
-  return 'rgb(99,191,215)';
-} else if (remainder === 3) {
-  return 'rgb(253,197,38)';
-} else 
-  return 'rgb(128,168,77)';
+  if (remainder === 0) {
+    return "rgb(221,70,60)";
+  } else if (remainder === 1) {
+    return "rgb(252,188,201)";
+  } else if (remainder === 2) {
+    return "rgb(99,191,215)";
+  } else if (remainder === 3) {
+    return "rgb(253,197,38)";
+  } else return "rgb(128,168,77)";
 }
 
 function openContactDetail(index) {
   let content = document.getElementById("contact-detail");
   content.innerHTML = " ";
-  let allContacts = usersArray[activeUser['userID']]["userContacts"];
+
+  let allContacts = usersArray[activeUser["userID"]]["userContacts"];
   let contact = allContacts[index];
   const { name, initials, initialsColor, email, phone } = contact;
   console.log(name, initials, initialsColor, email, phone);
-  
+
   content.innerHTML += `
   <div class="contact-detail-header">
   <div class="letters-large" style="background-color: ${initialsColor}">${initials}
@@ -118,19 +185,12 @@ function openContactDetail(index) {
 
   <div class="contact-body-header">
       <div class="contact-information">Contact Information</div>
-      <div class="edit-contact"><img src="../img/pencil_small.png">Edit Contact</div>
+      <div class="edit-contact" onclick="openEditContactDialog(${index})"><img  onclick="openEditContactDialog()"src="../img/pencil_small.png">Edit Contact</div>
   </div>
-  <div class="contact-email">Email</div>
-  <div>${email}</div>
-  <div class="contact-phone">Phone</div>
-  <div>894300289490500ß3</div>
+  <div class="contact-detail-bold">Email</div>
+  <div class="contact-detail-medium">${email}</div>
+  <div class="contact-detail-bold">Phone</div>
+  <div class="contact-detail-medium">894300289490500ß3</div>
 </div>
-
-
-
-
-      
       `;
-  
-
 }
