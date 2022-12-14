bad086b6a8e640c40a019b5b334f392c1a61e775
danijel991 <danijel.savkovic@yahoo.com>
2022-12-14 14:04:32 +0100

    updated add_task.css 14.12.22 14:04

---
+++ src/js/add_task.js
@@ -0,0 +1,11 @@
+// This function will change bg-color of urgency button
+changeUrgencyColor()
+
+// This function will add subtask
+addSubtask()
+
+// This function will add form-data
+addTask()
+
+// This function will clear form
+clearForm()
