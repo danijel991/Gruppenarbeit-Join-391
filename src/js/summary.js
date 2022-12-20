7bee1720994cc3b3fc8ad493f6446b0d500215fc
alsiesta <as@daslizenzatelier.de>
2022-12-20 22:29:12 +0100

    Update summary.js

--- src/js/summary.js
+++ src/js/summary.js
@@ -1,3 +1,6 @@
+const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
 function renderGreeting() {
     renderActiveUserName();
     renderDayGreeting();
@@ -12,6 +15,10 @@ function renderDayGreeting() {
     document.getElementById('day-time').innerHTML = greeting;
 }
 
+function renderTodaysDate()
+let (month, day, year) = createDate();
+document.getElementById('date-of-today').innerHTML = `${month} ' '${day} ', '${year}`; 
+
 function getDayTime() {
     let date = new Date();
     let hours = date.getHours();
@@ -23,3 +30,18 @@ return 'Good afternoon, '
         return 'Good evening, '
     }
 };
+
+function createDate() {
+    let date = new Date();
+    let day = date.getDate();
+    let month = months[date.getMonth()];
+    let year = date.getFullYear();
+    return (month, day, year);
+}
+
+// function getWeekday() {
+//     let weekday = new Date();
+//     let day = weekday.getDay();
+//     let today = weekdays[day];
+//     return today;
+// }
