71841c4e53eb328ab7147dde422c398206c8fb7d
alsiesta <as@daslizenzatelier.de>
2023-01-18 11:03:29 +0100

    did documentation in add_tasks.js

--- src/js/add_task.js
+++ src/js/add_task.js
@@ -1,14 +1,17 @@
 let selectedCategory;
 let selectedColor;
 
-// funktion, damit er versucht die kontakte bei onload zu befüllen
-
+/**
+ * function renders actual contacts of active user in drop-down menue of Add-Task Dialog
+ */
 async function init_add_task() {
     await init();
     renderContactsInDropDown();
 }
 
-// This function will change bg-color of urgency button
+/**
+ * function changes bg-color of urgency button
+ */
 function changeUrgencyHigh() {
     document.getElementById('urgency-btn-1').style.backgroundColor = "#FF3D00";
     document.getElementById('urgency-btn-1').style.color = "#FFFFFF";
@@ -17,32 +20,32 @@ function changeUrgencyHigh() {
     event.preventDefault();
 }
 
-// This function will clear form
+/**
+ * function clears form
+ */
 function clearForm() {
     document.getElementById('myForm').reset();
 }
 
+/**
+ * Function generates HTML within Category input field after a new category was created or an existing one was selected
+ * @param {string} category 
+ * @param {string} color 
+ */
 function selectCategory(category, color) {
-    // const color = {
-    //     pink: '#FC71FF',
-    //     turqoise: '#1FD7C1;',
-
-    // }
-
-    // let hex = colors[color];
-
-    // if (hex == undefined) {
-    //     hex = 'black'
-    // }
-
     document.getElementById('category-dropdown').innerHTML = '';
     document.getElementById('category-dropdown').innerHTML = category + `<div class="category-color ${color}"></div>`;
     document.getElementById('category-dropdown').classList.add('dropdown-active');
     document.getElementById(color).checked = true;
 };
 
-// Hier befindet sich die New Cagetory funktion, aus input feld name + farbe 
 
+/**
+ * 
+ * @param {string} category 
+ * @param {string} color 
+ * @returns HTML code that renders content in drop-down menue of Add-Task
+ */
 function generateCategoryHTML(category, color) {
     return `
     <div onclick="selectCategory('${category}','${color}')" class="dropdown-category" id="">
@@ -54,15 +57,13 @@ function generateCategoryHTML(category, color) {
     `;
 }
 
+/**
+ * reads user input in AddTask Dialog
+ */
 function addNewCategory() {
     let category = document.getElementById('category-input').value;
     let color = document.querySelector("input[type=radio][name=color]:checked").value;
-
-    console.log(category,color);
-
     document.getElementById('collapseCategory').innerHTML += generateCategoryHTML(category, color);
-
     closeCategoryInput();
-
     selectCategory(category, color);
 }
